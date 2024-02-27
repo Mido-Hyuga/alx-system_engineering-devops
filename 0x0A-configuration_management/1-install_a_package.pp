d1f2435f489fea76b20219cce5e4cdc209493ae4
root <elmehdibouslam@gmail.com>
2024-02-27 12:20:12 -0800

    configuration pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
