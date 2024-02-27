6d0dde4000ea4ed2194bf6776b789ea96cac29d0
root <elmehdibouslam@gmail.com>
2024-02-27 12:28:46 -0800

    configuration pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,14 @@
-#Create a file in /tmp
+# Your Puppet manifest file
 
-file { 'school':
-ensure  => 'present',
-path    => '/tmp/school',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet'
+# Define a class or node block here
+# ...
+
+# Create the file in /tmp/school
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',     # Set file permissions to 0744
+  owner   => 'www-data', # Set file owner to www-data
+  group   => 'www-data', # Set file group to www-data
+  content => 'I love Puppet',
 }
+
