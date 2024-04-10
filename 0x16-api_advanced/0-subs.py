4a2dadc4a7ef2423269e7dac4eb165b7652cc4f7
root <elmehdibouslam@gmail.com>
2024-04-10 04:39:08 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """initializate"""
-    if (type(subreddit) is not str):
+    if (type(subreddit) != str):
         return(0)
     url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
     headers = {'user-agent': 'safari:holberton/0.1.0'}
     response = requests.get(url_api, headers=headers)
-    if response.status_code is not 200:
+    if response.status_code != 200:
         return(0)
     return(response.json().get("data").get("subscribers"))
